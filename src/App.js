df93cb011c5d25a9b7656fac896e5141ea741e37
Konrad Pazdalski <konrad3021@gmail.com>
2023-07-06 08:23:56 +0200

    Hash router fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./index.css";
 import Game from "./pages/Game/Game";
 import Menu from "./pages/Menu/Menu";
@@ -10,30 +10,28 @@ function App() {
 
   return (
     <div className="App">
-      <HashRouter basename="/">
-        <Routes>
-          <Route
-            path="/game"
-            element={
-              <Game
-                botReactionTimeChoice={botReactionTimeChoice}
-                isSoundOn={isSoundOn}
-              />
-            }
-          />
-          <Route
-            path="/"
-            element={
-              <Menu
-                setBotReactionTimeChoice={setBotReactionTimeChoice}
-                botReactionTimeChoice={botReactionTimeChoice}
-                setIsSoundOn={setIsSoundOn}
-                isSoundOn={isSoundOn}
-              />
-            }
-          />
-        </Routes>
-      </HashRouter>
+      <Routes>
+        <Route
+          path="/game"
+          element={
+            <Game
+              botReactionTimeChoice={botReactionTimeChoice}
+              isSoundOn={isSoundOn}
+            />
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <Menu
+              setBotReactionTimeChoice={setBotReactionTimeChoice}
+              botReactionTimeChoice={botReactionTimeChoice}
+              setIsSoundOn={setIsSoundOn}
+              isSoundOn={isSoundOn}
+            />
+          }
+        />
+      </Routes>
     </div>
   );
 }
